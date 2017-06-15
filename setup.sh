0df33aa29d887fead1847aed6692ff4b9bf2e1f0
Alexander Teno <alexander.teno@thinkresearch.com>
2017-06-15 13:11:02 -0400

    Introduce start-server script for convenience, improve setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# TODO: Make platform independent
+# TODO: Branch if installed
+wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
+rm erlang-solutions_1.0_all.deb
+sudo apt-get update
+sudo apt-get install esl-erlang
+sudo apt-get install elixir
+
 echo y | mix local.hex
 echo y | mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez
 mix local.rebar --force
+
+pushd status_monitor
+  mix deps.get
+  mix ecto.create
+popd
---
+++ start-server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pushd status_monitor
+	mix phoenix.server
+popd
